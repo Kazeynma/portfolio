fc9ba1bd7f5c72dc8e8d89774ef2456d558f2a72
Kagnana <kagnana.ith@ecole-isitech.fr>
2023-10-05 16:35:02 +0200

    update Competence view

--- src/components/CompetenceCard.tsx
+++ src/components/CompetenceCard.tsx
@@ -1,6 +1,5 @@
 import {
-    Card,
-    CardContent,
+    Card, CardContent,
     CardHeader,
     CardTitle,
 } from "../@/components/ui/card"
@@ -9,20 +8,22 @@ import {
 type Props = {
     img?: string
     title: string
-    content?: JSX.Element
+    content?: string
 }
 
 const CompetenceCard = (props: Props) => {
     return (
-        <div>
-            <Card className={"w-96 h-full"}>
+        <div className={"relative"}>
+            <Card className={"w-40 h-32"}>
                 <CardHeader>
                     <CardTitle className={"flex flex-col items-center gap-2"}>
-                        <img src={props.img} className={"h-28"}/>
+                        <img src={props.img} className={"h-8"}/>
                         {props.title}
                     </CardTitle>
                 </CardHeader>
-                <CardContent className={"flex flex-col"}>
+            </Card>
+            <Card className={"w-40 h-32 absolute opacity-0 top-0 bottom-0 right-0 left-0 hover:opacity-100 hover:animate-fade hover:animate-ease-in-outr"}>
+                <CardContent className={"flex justify-center items-center px-1 py-0 h-full text-center"}>
                     {props.content}
                 </CardContent>
             </Card>
--- src/views/Competences.tsx
+++ src/views/Competences.tsx
@@ -6,69 +6,70 @@ import CompetenceCard from "../components/CompetenceCard.tsx";
 import reactIcon from "../assets/React-icon.svg.png"
 import jsLogo from "../assets/JavaScript-logo.png"
 import tsLogo from "../assets/TypeScript-logo.png"
-import javaLogo from "../assets/Java_Logo.svg.png"
+import javaLogo from "../assets/Java_Logo.svg"
+import swiftLogo from "../assets/swift.png"
+import swiftUiLogo from "../assets/swiftui.png"
 import gitLogo from "../assets/Git-logo.png"
 import dockerLogo from "../assets/docker-logo.svg"
 
 const Competences = () => {
     return (
-        <div id={"competences"} className={"min-h-screen flex justify-center items-center flex-col gap-5"}>
-            <span className={"text-3xl"}>Mes compétences</span>
+        <div className={"min-h-screen flex justify-center items-center flex-col gap-5 p-10"}>
+            <h2 id={"competences"} className={"title-section"}>Mes compétences</h2>
             <section id={"langage"} className={"flex flex-col w-4/5"}>
-                <span className={"my-2 text-xl"}>Langage</span>
-                <div className={"flex flex-wrap justify-evenly"}>
+                <span className={"text-lg"}>Langage</span>
+                <div className={"flex flex-wrap justify-center gap-5"}>
                     <CompetenceCard
                         title={"Javascript"}
                         img={jsLogo}
-                        content={
-                            <p className={"text-center"}>Utilisation dans le cadre professionnel et personnel</p>
-                        }
+                        content={"Utilisation dans le cadre professionnel et personnel"}
                     />
                     <CompetenceCard
                         title={"Typescript"}
                         img={tsLogo}
-                        content={
-                            <p className={"text-center"}>Utilisation dans le cadre de projet</p>
-                        }
+                        content={"Utilisation dans le cadre de projet"}
                     />
                     <CompetenceCard
                         title={"Java"}
                         img={javaLogo}
-                        content={
-                        <p className={"text-center"}>Utilisation dans le cadre de projet</p>
-                    }
+                        content={"Utilisation dans le cadre de projet"}
+                    />
+                    <CompetenceCard
+                        title={"Swift"}
+                        img={swiftLogo}
+                        content={"Utilisation dans le cadre professionnel"}
                     />
                 </div>
             </section>
             <Separator className={"w-2/3"}/>
             <section id={"framework"} className={"flex flex-col w-4/5"}>
-                <span className={"my-2 text-xl"}>Framework/Librairie</span>
-                <div className={"flex flex-wrap justify-evenly"}>
+                <span className={"text-lg"}>Framework/Librairie</span>
+                <div className={"flex flex-wrap justify-center gap-5"}>
                     <CompetenceCard
                         title={"ReactJS"}
                         img={reactIcon}
-                        content={
-                            <p className={"text-center"}>Utilisation dans le cadre professionnel et personnel</p>
-                        }
+                        content={"Utilisation dans le cadre professionnel et personnel"}
+                    />
+                    <CompetenceCard
+                        title={"SwiftUI"}
+                        img={swiftUiLogo}
+                        content={"Utilisation dans le cadre professionnel"}
                     />
                 </div>
             </section>
+            <Separator className={"w-2/3"}/>
             <section id={"outil"} className={"flex flex-col w-4/5"}>
-                <span className={"my-2 text-xl"}>Outils</span>
-                <div className={"flex flex-wrap justify-evenly"}>
+                <span className={"text-lg"}>Outils</span>
+                <div className={"flex flex-wrap justify-center gap-5"}>
                     <CompetenceCard
                         title={"Git"}
                         img={gitLogo}
-                        content={
-                            <p className={"text-center"}>Utilisation dans tous mes projets avec Github</p>
-                        }
+                        content={"Utilisation dans tous mes projets avec Github"}
                     />
                     <CompetenceCard
                         title={"Docker"}
                         img={dockerLogo}
-                        content={
-                            <p className={"text-center"}>Apprentissage de la technologie</p>
-                        }
+                        content={"Apprentissage de la technologie"}
                     />
                 </div>
             </section>
